b442c6fadc5d58a83deee5694096bb211619942e
Kamil Trzciński <ayufan@ayufan.eu>
2022-11-15 21:09:14 +0100

    Move RTSSSharedMemoryNET to `CommonHelpers`

---
+++ CommonHelpers/OSDHelpers.cs
@@ -0,0 +1,26 @@
+﻿using RTSSSharedMemoryNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonHelpers
+{
+    public static class OSDHelpers
+    {
+        public static uint OSDIndex(this OSD? osd)
+        {
+            if (osd is null)
+                return uint.MaxValue;
+
+            var osdSlot = typeof(OSD).GetField("m_osdSlot",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var value = osdSlot.GetValue(osd);
+            if (value is null)
+                return uint.MaxValue;
+
+            return (uint)value;
+        }
+    }
+}
