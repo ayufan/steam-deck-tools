c243d0dd698c7ac6ec78b13bde2a3ff4e8c0db97
Kamil Trzciński <ayufan@ayufan.eu>
2022-11-27 15:32:33 +0100

    Add some various helper development scripts

--- scripts/run_loop.bat
+++ scripts/run_loop.bat
@@ -1,15 +1,15 @@
 if not "%1"=="am_admin" (powershell start -verb runas '%0' 'am_admin,%1' & exit /b)
 if "%2"=="" (echo missing name & timeout /t 3 & exit /b)
 
-cd "%~dp0"
+cd "%~dp0\.."
 
 :retry
 
-taskkill /F /IM "%1.exe"
-del %1\bin\Debug\net6.0-windows\%1.exe
+taskkill /F /IM "%2.exe"
+del %2\bin\Debug\net6.0-windows\%2.exe
 
 dotnet build
-%1\bin\Debug\net6.0-windows\%1.exe
+%2\bin\Debug\net6.0-windows\%2.exe
 
 timeout /t 3
 goto retry
---
+++ scripts/run_loop_PowerControl.bat
@@ -0,0 +1,1 @@
+run_loop.bat PowerControl
