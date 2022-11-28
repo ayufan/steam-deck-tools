7c559e58f7cbc1a270179a8b4ed64842bbac0897
Kamil Trzciński <ayufan@ayufan.eu>
2022-11-28 11:04:53 +0100

    Do not log to Trace in DEBUG

--- CommonHelpers/Log.cs
+++ CommonHelpers/Log.cs
@@ -4,12 +4,22 @@ namespace CommonHelpers
 {
     public static class Log
     {
+#if DEBUG
+        private static bool LogToTrace = true;
+#else
+        private static bool LogToTrace = false;
+#endif
+        private static bool LogToConsole = Environment.UserInteractive;
+
         public static void TraceLine(string format, params object?[] arg)
         {
-            String line = string.Format(format, arg);
+            if (!LogToTrace && !LogToConsole)
+                return;
 
-            Trace.WriteLine(line);
-            if (Environment.UserInteractive)
+            String line = string.Format(format, arg);
+            if (LogToTrace)
+                Trace.WriteLine(line);
+            if (LogToConsole)
                 Console.WriteLine(line);
         }
     }
