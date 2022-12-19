63a98d067f56403d5379024dac471d547b26c04b
Kamil Trzciński <ayufan@ayufan.eu>
2022-12-19 23:45:44 +0100

    PowerControl: Provide `ValueChanged` event

--- PowerControl/Controller.cs
+++ PowerControl/Controller.cs
@@ -62,6 +62,7 @@ namespace PowerControl
 
             var contextMenu = new System.Windows.Forms.ContextMenuStrip(components);
 
+            rootMenu.Init();
             rootMenu.Visible = false;
             rootMenu.Update();
             rootMenu.CreateMenu(contextMenu);
